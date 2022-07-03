04b0283cbf74e73249f280ee5fa585a511a402a9
--global <liman.viacheslav@gmail.com>
2022-07-03 15:58:03 +0300

    TSA-37: Implement home page

--- src/components/Pages/HomePage.vue
+++ src/components/Pages/HomePage.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="page_wrapper">
-    <h1>Heading</h1>
-    <p>Text</p>
+    <div class="block_wrapper">
+      <div class="img_block">
+        <img src="@/assets/images/Banner1.png" alt="Bamnner1"/>
+      </div>
+      <div>
+        <h3>Heading</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
+    <div>
+      
+    </div>
   </div>
 </template>
 
@@ -11,6 +24,20 @@ export default {
 };
 </script>
 
-<style>
+<style local>
+  .page_wrapper {
+      margin-top: 75px;
+  }
 
+  .block_wrapper {
+      display: flex;
+  }
+
+  .img_block {
+      margin-right: 75px;
+  }
+  
+  .img_block img {
+      width: 600px;
+  }
 </style>
