8c2317ae3698cf272eded34c796ecffd96fc93fa
MaksymSolodovnykov <msol@rismasystems.com>
2022-07-07 23:31:25 +0300

    TSA-41: Added filter component and implemented base functionality

---
+++ src/components/Atoms/Closeable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div 
+      class="flex items-center justify-between"
+      @click="isContentVisible = !isContentVisible">
+      <h3>{{ title }}</h3>
+      <feather-icon
+        :icon="isContentVisible ? 'chevron-down' : 'chevron-up'"
+        class="stroke-2 text-blue-700x" />
+    </div>
+    <div v-if="isContentVisible">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import FeatherIcon from '@/components/Atoms/FeatherIcon';
+
+export default {
+    components: {
+        FeatherIcon
+    },
+    props: {
+        title: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        defaultVisibility: {
+            type: Boolean,
+            required: false,
+            default: true
+        }
+    },
+    data() {
+        return {
+            isContentVisible: true
+        };
+    },
+    mounted() {
+        this.isContentVisible = this.defaultVisibility;
+    },
+}
+</script>
